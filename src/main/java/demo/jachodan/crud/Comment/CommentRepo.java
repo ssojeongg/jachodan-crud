dbec523c456362cd2fff27ccbc4a179c9c159b81
2025035 <hslee33@wips.co.kr>
2025-09-11 17:54:10 +0900

    delete: clearing main branch

--- src/main/java/demo/jachodan/crud/Comment/CommentRepo.java
+++
@@ -1,6 +0,0 @@
-package demo.jachodan.crud.Comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepo extends JpaRepository<Comment, Integer> {
-}
--- src/main/java/demo/jachodan/crud/Post/PostRepo.java
+++
@@ -1,6 +0,0 @@
-package demo.jachodan.crud.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepo extends JpaRepository<Post, Integer> {
-}
